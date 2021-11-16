a22abc641ef5093da0e99897e236eb66544d020a
Karen Banci <karenbanci1@gmail.com>
2021-11-16 16:43:57 -0300

    terminei o projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new_tasks
 
+  # criar e mostrar a lista de tasks em uma nova janela
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+
+  # fazer um update na lista de tasks
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_tasks
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # deletar
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
 
 # Como usuário, posso listar tarefas
