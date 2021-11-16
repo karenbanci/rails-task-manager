1ddb26cf68c74f909fd15c95cd44733f5bb3a391
Karen Banci <karenbanci1@gmail.com>
2021-11-16 14:23:03 -0300

    criei a view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: :tasks
+
 end
+
+# Como usuário, posso listar tarefas
+# Como usuário, posso ver os detalhes de uma tarefa
+# Como usuário, posso adicionar uma nova tarefa
+# Como usuário, posso editar uma tarefa (marcar como concluída / atualizar título e detalhes)
+# Como usuário, posso remover uma tarefa
